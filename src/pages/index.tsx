c236112a5e47101279aecab019e0f694677eb29d
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2021-02-25 12:04:59 -0300

    Imported CountDownProvider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,12 @@ import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
 import { ChallengeBox } from "../components/ChallengeBox";
-
+import { CountDownProvider } from '../contexts/CountdownContext';
 
 
 
 import styles from '../styles/pages/Home.module.css'
+
 
 
 export default function Home() {
@@ -21,18 +22,20 @@ export default function Home() {
 
       <ExperienceBar/>
       
-      <section>
-        <div>
-          <Profile/>
-          <CompletedChallenges/>
-          <Countdown/>
-        </div>
 
-        <div>
-          <ChallengeBox/>
-        </div>
-      </section>
+      <CountDownProvider>
+        <section>
+          <div>
+            <Profile/>
+            <CompletedChallenges/>
+            <Countdown/>
+          </div>
 
+          <div>
+            <ChallengeBox/>
+          </div>
+        </section>
+      </CountDownProvider>
     </div>
     
 
