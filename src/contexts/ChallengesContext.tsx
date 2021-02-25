16de3ea865414e2a63a18cadace051e019d2d6e5
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2021-02-25 12:03:47 -0300

    challenge and notification permission, completed

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, ReactNode } from 'react';
+import { createContext, useState, ReactNode, useEffect } from 'react';
 import challenges from '../../challenges.json';
 
 interface Challenge{
@@ -18,6 +18,7 @@ interface ChallengesContextData{
   levelUp: () => void;
   startNewChallenge: ()=> void;
   resetChallenge: () => void;
+  completedChallenge: () => void;
 
 }
 
@@ -37,7 +38,11 @@ export function ChallengesProvider({ children }:ChallengesProviderProps ){
 
   const [activeChallenge, setActiveChallenge] = useState(null);
 
-  const experienceToNextLevel = Math.pow((level+1) * 4,2)
+  const experienceToNextLevel = Math.pow((level+1) * 4,2);
+
+  useEffect(()=> {
+    Notification.requestPermission();
+  }, [])
 
 
   function levelUp(){
@@ -49,10 +54,39 @@ export function ChallengesProvider({ children }:ChallengesProviderProps ){
     const challenge = challenges[randomChallengeIndex];
 
     setActiveChallenge(challenge);
+
+    new Audio('/notification.mp3').play();
+
+
+    if(Notification.permission === 'granted'){
+      new Notification('Novo desafio 🎉🎊',{
+        body: `Valendo ${challenge.amount}xp!`
+      })
+    }
   }
 
   function resetChallenge(){
     setActiveChallenge(null);
+  }
+
+  function completedChallenge(){
+    if(!activeChallenge){
+      return;
+    }
+
+    const { amount } = activeChallenge;
+
+    let finalExperience = currentExperience + amount;
+
+    if(finalExperience >= experienceToNextLevel){
+      finalExperience = finalExperience - experienceToNextLevel;
+      levelUp();
+    }
+
+    setCurrentExperience(finalExperience);
+    setActiveChallenge(null);
+    setChallengesCompleted(challengesCompleted + 1);
+
   }
 
 
@@ -67,6 +101,7 @@ export function ChallengesProvider({ children }:ChallengesProviderProps ){
        startNewChallenge,
        activeChallenge,
        resetChallenge,
+       completedChallenge,
 
        }}>
       {children}
